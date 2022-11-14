cbaa7741c0be2a7fed7730483258d63484e7a24c
Nick Shapoff <nicholasshapoff@gmail.com>
2022-11-14 12:17:42 -0800

    working!

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,55 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('test for the header', () => {
+  test('Test header renders with correct text', () => {
+    render(<App />);
+    const headerE1 = screen.getByRole("heading");
+    expect(headerE1.textContent).toBe("Testing Playground");
+  })
+})
+
+describe('test for the button', () => {
+  test('button changes color when clicked', () => {
+    render(<App />)
+    const colorBtn = screen.getByRole("button")
+  
+    userEvent.click(colorBtn)
+  
+    expect(colorBtn).toHaveStyle({backgroundColor: "blue"})
+    expect(colorBtn.textContent).toBe("Change button color to green")
+  })
+})
+
+describe('test for the checkbox', () => {
+  test('Checkbox works to facilitate change of button color', () => {
+    render (<App />)
+    const colorBtn = screen.getByRole("button")
+    const checkbox = screen.getByRole("checkbox")
+
+    userEvent.click(checkbox)
+    expect(colorBtn).toBeDisabled()
+
+    userEvent.click(checkbox)
+    expect(colorBtn).toBeEnabled()
+  })
+})
+
+
+describe('test if enabled text is working', () => {
+  test('Enabled display is working', () => {
+    render (<App />)
+    const checkbox = screen.getByRole("checkbox")
+    const paragraphE1 = screen.getByRole("paragraph")
+
+    expect(paragraphE1.textContent).toBe("Button is enabled")
+
+    userEvent.click(checkbox)
+    expect(paragraphE1.textContent).toBe("Button is disabled")
+
+    userEvent.click(checkbox)
+    expect(paragraphE1.textContent).toBe("Button is enabled")
+  })
+})
+
